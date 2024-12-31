1fcf607231281b9de0b326f9119415984480cc0b
David <David@192.168.1.53>
2024-12-31 14:18:45 +0100

    Declaramos las variables

---
+++ src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test;
+
+/**
+ *
+ * @author David
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        
+        
+        //Declaramos las variables
+        int n1, n2;
+        
+        
+    }
+}
