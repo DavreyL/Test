fdc5618c6bdafeba40cb9740a6f7b7f7c5f22d17
David <David@192.168.1.53>
2024-12-31 14:20:01 +0100

    Declaramos otra variable para meter los datos de n1 y n2

--- src/main/java/com/mycompany/test/Test.java
+++ src/main/java/com/mycompany/test/Test.java
@@ -16,6 +16,9 @@ public class Test {
         //Declaramos las variables
         int n1, n2;
         
+        //declaramos otra variable para meter los datos de n1 y n2
+        int suma;
+        
         
     }
 }
